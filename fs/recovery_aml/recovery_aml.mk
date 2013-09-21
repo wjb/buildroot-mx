05fa8824cf05ff083124385046fd235d99a8ec64
Stanislav Vlasic <svlasic@gmail.com>
2013-09-21 13:54:49 +0200

    [recovery_aml] Rewrite the code a little, add flash recovery option
    Conflicts:
    	fs/recovery_aml/recovery_aml.mk

--- fs/recovery_aml/recovery_aml.mk
+++ fs/recovery_aml/recovery_aml.mk
@@ -14,6 +14,15 @@ ifeq ($(BR2_TARGET_ROOTFS_RECOVERY_AML_WIPE_USERDATA_CONDITIONAL),y)
   RECOVERY_AML_ARGS += -c
 endif
 
+ifneq ($(strip $(BR2_TARGET_ROOTFS_RECOVERY_RECOVERY_IMG)),"")
+
+# Check if recovery.img exists
+  $(if $(wildcard $(BR2_TARGET_ROOTFS_RECOVERY_RECOVERY_IMG)),,$(fatal recovery.img does not exist (Path: $(BR2_TARGET_ROOTFS_RECOVERY_RECOVERY_IMG)).))
+
+  
+  RECOVERY_AML_ARGS += -r
+endif
+
 # Introduce imgpack
 ifeq ($(BR2_TARGET_ROOTFS_RECOVERY_AML_IMGPACK), y)
 
@@ -25,7 +34,10 @@ ifneq ($(strip $(BR2_TARGET_ROOTFS_RECOVERY_AML_IMGPACK_CUSTOM)),"")
 RES_PACK = $(BR2_TARGET_ROOTFS_RECOVERY_AML_IMGPACK_CUSTOM)
 endif
 
-RECOVERY_AML_ARGS += -l yes
+# Check if AML_IMGPACK folder exists
+$(if $(wildcard $(RES_PACK)),,$(fatal RES_PACK=$(RES_PACK), folder does not exist.))
+
+RECOVERY_AML_ARGS += -l
 
 endif
 
@@ -48,50 +60,50 @@ else
 AML_LOGO = fs/recovery_aml/aml_logo.img
 endif
 
-ifneq ($(BR2_TARGET_ROOTFS_RECOVERY_AML_IMGPACK),y)
+# Check if AML_LOGO exists
+$(if $(wildcard $(AML_LOGO)),,$(fatal AML_LOGO=$(AML_LOGO), file does not exist.))
 
-define ROOTFS_RECOVERY_AML_CMD
- mkdir -p $(BINARIES_DIR)/aml_recovery/system && \
- tar -C $(BINARIES_DIR)/aml_recovery/system -xf $(BINARIES_DIR)/rootfs.tar && \
- mkdir -p $(BINARIES_DIR)/aml_recovery/META-INF/com/google/android/ && \
- PYTHONDONTWRITEBYTECODE=1 $(HOST_DIR)/usr/bin/python fs/recovery_aml/android_scriptgen $(RECOVERY_AML_ARGS) -i -p $(BINARIES_DIR)/aml_recovery/system -o \
-   $(BINARIES_DIR)/aml_recovery/META-INF/com/google/android/updater-script && \
- cp -f fs/recovery_aml/update-binary $(BINARIES_DIR)/aml_recovery/META-INF/com/google/android/ && \
- cp -f fs/recovery_aml/aml_logo.img $(BINARIES_DIR)/aml_recovery/ && \
- cp -f $(BINARIES_DIR)/uImage $(BINARIES_DIR)/aml_recovery/ && \
- find $(BINARIES_DIR)/aml_recovery/system/ -type l -delete && \
- find $(BINARIES_DIR)/aml_recovery/system/ -type d -empty -exec sh -c 'echo "dummy" > "{}"/.empty' \; && \
- pushd $(BINARIES_DIR)/aml_recovery/ >/dev/null && \
- zip -m -q -r -y $(BINARIES_DIR)/aml_recovery/update-unsigned.zip aml_logo.img uImage META-INF system && \
- popd >/dev/null && \
- echo "Signing $(UPDATE_ZIP)..." && \
- pushd fs/recovery_aml/ >/dev/null; java -Xmx1024m -jar signapk.jar -w testkey.x509.pem testkey.pk8 $(BINARIES_DIR)/aml_recovery/update-unsigned.zip $(BINARIES_DIR)/$(UPDATE_ZIP) && \
- rm -rf $(BINARIES_DIR)/aml_recovery; rm -f $(TARGET_DIR)/usr.sqsh
-endef
+# Aditional files to be included in package, by default only aml_logo.img
+ADDITIONAL_FILES = aml_logo.img
 
-else
+ROOTFS_RECOVERY_AML_CMD = \
+    mkdir -p $(BINARIES_DIR)/aml_recovery/system &&
 
-define ROOTFS_RECOVERY_AML_CMD
- mkdir -p $(BINARIES_DIR)/aml_recovery/system && \
- echo "Creating logo.img..." && \
- fs/recovery_aml/imgpack -r $(RES_PACK) $(BINARIES_DIR)/aml_recovery/logo.img && \
- tar -C $(BINARIES_DIR)/aml_recovery/system -xf $(BINARIES_DIR)/rootfs.tar && \
- mkdir -p $(BINARIES_DIR)/aml_recovery/META-INF/com/google/android/ && \
- PYTHONDONTWRITEBYTECODE=1 $(HOST_DIR)/usr/bin/python fs/recovery_aml/android_scriptgen $(RECOVERY_AML_ARGS) -i -p $(BINARIES_DIR)/aml_recovery/system -o \
-   $(BINARIES_DIR)/aml_recovery/META-INF/com/google/android/updater-script && \
- cp -f fs/recovery_aml/update-binary $(BINARIES_DIR)/aml_recovery/META-INF/com/google/android/ && \
- cp -f $(AML_LOGO) $(BINARIES_DIR)/aml_recovery/aml_logo.img && \
- cp -f $(BINARIES_DIR)/uImage $(BINARIES_DIR)/aml_recovery/ && \
- find $(BINARIES_DIR)/aml_recovery/system/ -type l -delete && \
- find $(BINARIES_DIR)/aml_recovery/system/ -type d -empty -exec sh -c 'echo "dummy" > "{}"/.empty' \; && \
- pushd $(BINARIES_DIR)/aml_recovery/ >/dev/null && \
- zip -m -q -r -y $(BINARIES_DIR)/aml_recovery/update-unsigned.zip logo.img aml_logo.img uImage META-INF system && \
- popd >/dev/null && \
- echo "Signing $(UPDATE_ZIP)..." && \
- pushd fs/recovery_aml/ >/dev/null; java -Xmx1024m -jar signapk.jar -w testkey.x509.pem testkey.pk8 $(BINARIES_DIR)/aml_recovery/update-unsigned.zip $(BINARIES_DIR)/$(UPDATE_ZIP) && \
- rm -rf $(BINARIES_DIR)/aml_recovery; rm -f $(TARGET_DIR)/usr.sqsh
-endef
+# If we use imgpack, append ROOTFS_RECOVERY_AML_CMD with aditional commands
+ifeq ($(BR2_TARGET_ROOTFS_RECOVERY_AML_IMGPACK),y)
 
+ROOTFS_RECOVERY_AML_CMD += \
+    echo "Creating logo.img..." && \
+    fs/recovery_aml/imgpack -r $(RES_PACK) $(BINARIES_DIR)/aml_recovery/logo.img && 
+
+ADDITIONAL_FILES += " logo.img"
 endif
+
+# If we have provided recovery.img, make sure it's included in update.zip
+ifneq ($(strip $(BR2_TARGET_ROOTFS_RECOVERY_RECOVERY_IMG)),"")
+
+ROOTFS_RECOVERY_AML_CMD += \
+    echo "Copy recovery.img..." && \
+    cp -f $(BR2_TARGET_ROOTFS_RECOVERY_RECOVERY_IMG) $(BINARIES_DIR)/aml_recovery/recovery.img && 
+
+ADDITIONAL_FILES += " recovery.img"
+endif
+
+ROOTFS_RECOVERY_AML_CMD += \
+    tar -C $(BINARIES_DIR)/aml_recovery/system -xf $(BINARIES_DIR)/rootfs.tar && \
+    mkdir -p $(BINARIES_DIR)/aml_recovery/META-INF/com/google/android/ && \
+    PYTHONDONTWRITEBYTECODE=1 $(HOST_DIR)/usr/bin/python fs/recovery_aml/android_scriptgen $(RECOVERY_AML_ARGS) -i -p $(BINARIES_DIR)/aml_recovery/system -o \
+     $(BINARIES_DIR)/aml_recovery/META-INF/com/google/android/updater-script && \
+    cp -f fs/recovery_aml/update-binary $(BINARIES_DIR)/aml_recovery/META-INF/com/google/android/ && \
+    cp -f $(AML_LOGO) $(BINARIES_DIR)/aml_recovery/aml_logo.img && \
+    cp -f $(BINARIES_DIR)/uImage $(BINARIES_DIR)/aml_recovery/ && \
+    find $(BINARIES_DIR)/aml_recovery/system/ -type l -delete && \
+    find $(BINARIES_DIR)/aml_recovery/system/ -type d -empty -exec sh -c 'echo "dummy" > "{}"/.empty' \; && \
+    pushd $(BINARIES_DIR)/aml_recovery/ >/dev/null && \
+    zip -m -q -r -y $(BINARIES_DIR)/aml_recovery/update-unsigned.zip $(ADDITIONAL_FILES) uImage META-INF system && \
+    popd >/dev/null && \
+    echo "Signing $(UPDATE_ZIP)..." && \
+    pushd fs/recovery_aml/ >/dev/null; java -Xmx1024m -jar signapk.jar -w testkey.x509.pem testkey.pk8 $(BINARIES_DIR)/aml_recovery/update-unsigned.zip $(BINARIES_DIR)/$(UPDATE_ZIP) && \
+    rm -rf $(BINARIES_DIR)/aml_recovery; rm -f $(TARGET_DIR)/usr.sqsh
 
 $(eval $(call ROOTFS_TARGET,recovery_aml))
