111da04e090ac58b369653c9084b0953b8486a58
wjb <jaybob413@gmail.com>
2013-11-23 17:22:43 -0500

    Move libamplayer to binary distribution
    based on Pivosgroup/buildroot-linux commmits:
    	a8d76b4f0758da9d7a8c355a0ddeb3b0b1ebcf38
    	09461363544e00347ab60068ccb49dc2435dee61

--- package/amlogic/libamplayerm3/libamplayerm3.mk
+++ package/amlogic/libamplayerm3/libamplayerm3.mk
@@ -3,17 +3,12 @@
 # libamplayer
 #
 #############################################################
-LIBAMPLAYERM3_VERSION:=4e428c7a65ef2db6a5a0a0c8cd467c2807a16c95
-LIBAMPLAYERM3_SITE=git://github.com/Stane1983/libamplayer-m3.git
+LIBAMPLAYERM3_VERSION:=e690701dbe22a79a2ed55953031713013e5d7d7e
+LIBAMPLAYERM3_SOURCE=libamplayerm3-$(LIBAMPLAYERM3_VERSION).tar.gz
+LIBAMPLAYERM3_SITE=$(TOPDIR)/package/amlogic/libamplayerm3/src
+LIBAMPLAYERM3_SITE_METHOD=local
 LIBAMPLAYERM3_INSTALL_STAGING=YES
 LIBAMPLAYERM3_INSTALL_TARGET=YES
-LIBAMPLAYERM3_SITE_METHOD=git
-
-ifeq ($(BR2_BOARD_TYPE_AMLOGIC_M6),y)
-FIRMWARE_FOLDER=firmware-m6
-else
-FIRMWARE_FOLDER=firmware
-endif
 
 ifeq ($(BR2_PACKAGE_LIBAMPLAYERM3),y)
 # actually required for amavutils and amffmpeg
@@ -24,41 +19,43 @@ AMFFMPEG_EXTRA_LDFLAGS += --extra-ldflags="-lamavutils"
 endif
 
 define LIBAMPLAYERM3_BUILD_CMDS
- $(call AMAVUTILS_BUILD_CMDS)
  $(call AMAVUTILS_INSTALL_STAGING_CMDS)
  $(call AMFFMPEG_CONFIGURE_CMDS)
  $(call AMFFMPEG_BUILD_CMDS)
  $(call AMFFMPEG_INSTALL_STAGING_CMDS)
-
- mkdir -p $(STAGING_DIR)/usr/include/amlplayer
- $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" HEADERS_DIR="$(STAGING_DIR)/usr/include/amlplayer" \
-  CROSS_PREFIX="$(TARGET_CROSS)" SYSROOT="$(STAGING_DIR)" PREFIX="$(STAGING_DIR)/usr" -C $(@D)/amadec install
- $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" HEADERS_DIR="$(STAGING_DIR)/usr/include/amlplayer" CROSS_PREFIX="$(TARGET_CROSS)" \
-  SYSROOT="$(STAGING_DIR)" PREFIX="$(STAGING_DIR)/usr" SRC=$(@D)/amcodec -C $(@D)/amcodec install
- $(MAKE) CROSS="$(TARGET_CROSS)" CC="$(TARGET_CC)" LD="$(TARGET_LD)" PREFIX="$(STAGING_DIR)/usr" \
-  SRC="$(@D)/amplayer" -C $(@D)/amplayer
 endef
 
 define LIBAMPLAYERM3_INSTALL_STAGING_CMDS
- $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" INSTALL_DIR="$(STAGING_DIR)/usr/lib" \
-  STAGING="$(STAGING_DIR)/usr" PREFIX="$(STAGING_DIR)/usr" -C $(@D)/amplayer install
+# install base includes
+	mkdir -p $(STAGING_DIR)/usr/include
+	install -m 644 $(@D)/usr/include/*.h $(STAGING_DIR)/usr/include
+# install includes to amlplayer
+	mkdir -p $(STAGING_DIR)/usr/include/amlplayer
+	install -m 644 $(@D)/usr/include/amlplayer/*.h $(STAGING_DIR)/usr/include/amlplayer
+	mkdir -p $(STAGING_DIR)/usr/include/amlplayer/amports
+	install -m 644 $(@D)/usr/include/amlplayer/amports/*.h $(STAGING_DIR)/usr/include/amlplayer/amports
+	mkdir -p $(STAGING_DIR)/usr/include/amlplayer/ppmgr
+	install -m 644 $(@D)/usr/include/amlplayer/ppmgr/*.h $(STAGING_DIR)/usr/include/amlplayer/ppmgr
+# install libs
+	mkdir -p $(STAGING_DIR)/usr/lib
+	install -m 755 $(@D)/usr/lib/*.so* $(STAGING_DIR)/usr/lib
+    if [ -e $(STAGING_DIR)/usr/lib/libamcodec.so ]; then rm $(STAGING_DIR)/usr/lib/libamcodec.so; fi;
+    cd $(STAGING_DIR)/usr/lib; ln -s libamcodec.so.0.0 libamcodec.so
 
- #temporary, until we sync with mainline xbmc
- cp -rf $(@D)/amcodec/include/* $(STAGING_DIR)/usr/include
+#temporary, until we sync with mainline xbmc
+	cp -rf $(@D)/usr/include/amlplayer/* $(STAGING_DIR)/usr/include
 endef
 
 define LIBAMPLAYERM3_INSTALL_TARGET_CMDS
- $(call AMAVUTILS_INSTALL_TARGET_CMDS)
- $(call AMFFMPEG_INSTALL_TARGET_CMDS)
+	$(call AMAVUTILS_INSTALL_TARGET_CMDS)
+	$(call AMFFMPEG_INSTALL_TARGET_CMDS)
 
- mkdir -p $(TARGET_DIR)/lib/firmware
- cp -rf $(@D)/amadec/$(FIRMWARE_FOLDER)/*.bin $(TARGET_DIR)/lib/firmware
- cp -f $(STAGING_DIR)/usr/lib/libamadec.so $(TARGET_DIR)/usr/lib/
-
- cp -f $(STAGING_DIR)/usr/lib/libamcodec.so.* $(TARGET_DIR)/usr/lib/
- cp -f $(STAGING_DIR)/usr/lib/libamplayer.so $(TARGET_DIR)/usr/lib/
- $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" INSTALL_DIR="$(TARGET_DIR)/usr/lib" \
-  STAGING="$(TARGET_DIR)/usr" PREFIX="$(STAGING_DIR)/usr" -C $(@D)/amplayer install
+	mkdir -p $(TARGET_DIR)/lib/firmware
+	install -m 644 $(@D)/lib/firmware/*.bin $(TARGET_DIR)/lib/firmware
+	mkdir -p $(TARGET_DIR)/usr/lib
+	install -m 755 $(@D)/usr/lib/*.so* $(TARGET_DIR)/usr/lib
+    if [ -e $(TARGET_DIR)/usr/lib/libamcodec.so ]; then rm $(TARGET_DIR)/usr/lib/libamcodec.so; fi;
+	cd $(TARGET_DIR)/usr/lib; ln -s libamcodec.so.0.0 libamcodec.so
 endef
 
 $(eval $(call generic-package))
