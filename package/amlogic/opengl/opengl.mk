346b3bbc43aff6fb363a4c816620dd8a5d874953
wjb <jaybob413@gmail.com>
2014-02-23 15:18:01 -0500

    [libamplayerm6] update to support opengl version for apiv20

--- package/amlogic/opengl/opengl.mk
+++ package/amlogic/opengl/opengl.mk
@@ -3,7 +3,27 @@
 # opengl
 #
 #############################################################
+
+# Set initial value to "none", if it stays that way, report an error
+OPENGL_VERSION=none
+
+ifeq ($(BR2_PACKAGE_OPENGL_API20),y)
+OPENGL_VERSION=apiv20
+endif
+
+ifeq ($(BR2_PACKAGE_OPENGL_API17),y)
 OPENGL_VERSION=apiv17
+endif
+
+ifeq ($(BR2_PACKAGE_OPENGL_API14),y)
+OPENGL_VERSION=apiv14
+endif
+
+# If API version is not selected, report an error
+ifeq ($(OPENGL_VERSION),none)
+$(fatal Error: No OpenGL version selected.)
+endif
+
 OPENGL_SOURCE=opengl-$(OPENGL_VERSION).tar
 OPENGL_SITE=$(TOPDIR)/package/amlogic/opengl/
 OPENGL_SITE_METHOD=file
