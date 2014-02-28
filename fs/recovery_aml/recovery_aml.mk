28f4a967e20d270088cdaf9544857359c7735516
wjb <jaybob413@gmail.com>
2014-02-28 15:56:11 -0500

    [recovery_aml] align with corey/stane

--- fs/recovery_aml/recovery_aml.mk
+++ fs/recovery_aml/recovery_aml.mk
@@ -14,6 +14,15 @@ ifeq ($(BR2_TARGET_ROOTFS_RECOVERY_AML_WIPE_USERDATA_CONDITIONAL),y)
   RECOVERY_AML_ARGS += -c
 endif
 
+ifneq ($(strip $(BR2_TARGET_ROOTFS_RECOVERY_BOOTLOADER_IMG)),"")
+
+# Check if bootloader.img exists
+  $(if $(wildcard $(BR2_TARGET_ROOTFS_RECOVERY_BOOTLOADER_IMG)),,$(fatal bootloader.img does not exist (Path: $(BR2_TARGET_ROOTFS_RECOVERY_BOOTLOADER_IMG)).))
+
+  
+  RECOVERY_AML_ARGS += -u
+endif
+
 ifneq ($(strip $(BR2_TARGET_ROOTFS_RECOVERY_RECOVERY_IMG)),"")
 
 # Check if recovery.img exists
@@ -26,27 +35,16 @@ endif
 # Introduce imgpack
 ifeq ($(BR2_TARGET_ROOTFS_RECOVERY_AML_IMGPACK), y)
 
-# We set default folder for res_pack resources
-RES_PACK = fs/recovery_aml/res_pack
-
-# Change it if custom folder is specified
+# We set folder for res_pack resources
 ifneq ($(strip $(BR2_TARGET_ROOTFS_RECOVERY_AML_IMGPACK_CUSTOM)),"")
 RES_PACK = $(BR2_TARGET_ROOTFS_RECOVERY_AML_IMGPACK_CUSTOM)
+else
+RES_PACK = fs/recovery_aml/logos/amlinux_res_pack
 endif
 
 # Check if AML_IMGPACK folder exists
 $(if $(wildcard $(RES_PACK)),,$(fatal RES_PACK=$(RES_PACK), folder does not exist.))
 
-RECOVERY_AML_ARGS += -l
-
-endif
-
-ifneq ($(strip $(BR2_TARGET_ROOTFS_RECOVERY_BOOTLOADER_IMG)),"")
-
-# Check if bootloader.img exists
-  $(if $(wildcard $(BR2_TARGET_ROOTFS_RECOVERY_BOOTLOADER_IMG)),,$(fatal bootloader.img does not exist (Path: $(BR2_TARGET_ROOTFS_RECOVERY_BOOTLOADER_IMG)).))
-
-  RECOVERY_AML_ARGS += -u
 endif
 
 # Check for UPDATE_ZIP_PREFIX to override file name
@@ -57,33 +55,27 @@ else
     UPDATE_ZIP_PREFIX = $(BR2_TARGET_ROOTFS_RECOVERY_AML_BOARDNAME)
 endif
 
+ifeq ($(BR2_TARGET_ROOTFS_RECOVERY_AML_UPDATE_IMG),y)
+  UPDATE_FORMAT = img
+else
+  UPDATE_FORMAT = zip
+endif
+
 ifeq ($(BR2_TARGET_ROOTFS_RECOVERY_AML_UPDATE_ZIP_NAME_FULL_DATETIME),y)
-    UPDATE_ZIP = $(UPDATE_ZIP_PREFIX)-$(shell date -u +%0d%^b%Y-%H%M%S)-update.zip
+    UPDATE_ZIP = $(UPDATE_ZIP_PREFIX)-$(shell date -u +%0d%^b%Y-%H%M%S)-update.$(UPDATE_FORMAT)
 endif
 ifeq ($(BR2_TARGET_ROOTFS_RECOVERY_AML_UPDATE_ZIP_NAME_SHORT_DATE),y)
-    UPDATE_ZIP = $(UPDATE_ZIP_PREFIX)-$(shell date -u +%Y%m%d)-update.zip
+    UPDATE_ZIP = $(UPDATE_ZIP_PREFIX)-$(shell date -u +%Y%m%d)-update.$(UPDATE_FORMAT)
 endif
 ifeq ($(BR2_TARGET_ROOTFS_RECOVERY_AML_UPDATE_ZIP_NAME_BOARDNAME_UPDATE_ZIP),y)
-    UPDATE_ZIP = $(UPDATE_ZIP_PREFIX)-update.zip
+    UPDATE_ZIP = $(UPDATE_ZIP_PREFIX)-update.$(UPDATE_FORMAT)
 endif
 ifeq ($(BR2_TARGET_ROOTFS_RECOVERY_AML_UPDATE_ZIP_NAME_UPDATE_ZIP),y)
-    UPDATE_ZIP = update.zip
+    UPDATE_ZIP = update.$(UPDATE_FORMAT)
 endif
 ifeq ($(BR2_TARGET_ROOTFS_RECOVERY_AML_UPDATE_ZIP_NAME_CUSTOM),y)
-    UPDATE_ZIP = $(BR2_TARGET_ROOTFS_RECOVERY_AML_UPDATE_ZIP_NAME_CUSTOM_STRING)-update.zip
+    UPDATE_ZIP = $(BR2_TARGET_ROOTFS_RECOVERY_AML_UPDATE_ZIP_NAME_CUSTOM_STRING)-update.$(UPDATE_FORMAT)
 endif
-
-ifneq ($(strip $(BR2_TARGET_ROOTFS_RECOVERY_AML_LOGO)),"")
-AML_LOGO = $(BR2_TARGET_ROOTFS_RECOVERY_AML_LOGO)
-else
-AML_LOGO = fs/recovery_aml/aml_logo.img
-endif
-
-# Check if AML_LOGO exists
-$(if $(wildcard $(AML_LOGO)),,$(fatal AML_LOGO=$(AML_LOGO), file does not exist.))
-
-# Aditional files to be included in package, by default only aml_logo.img
-ADDITIONAL_FILES = aml_logo.img
 
 ROOTFS_RECOVERY_AML_CMD = \
     mkdir -p $(BINARIES_DIR)/aml_recovery/system &&
@@ -91,28 +83,61 @@ ROOTFS_RECOVERY_AML_CMD = \
 # If we use imgpack, append ROOTFS_RECOVERY_AML_CMD with aditional commands
 ifeq ($(BR2_TARGET_ROOTFS_RECOVERY_AML_IMGPACK),y)
 
+ifeq ($(BR2_TARGET_ROOTFS_RECOVERY_AML_IMGPACK_1225),y)
+IMGPACK = imgpack-1225
+else
+IMGPACK = imgpack
+endif
+
 ROOTFS_RECOVERY_AML_CMD += \
     echo "Creating logo.img..." && \
-    fs/recovery_aml/imgpack -r $(RES_PACK) $(BINARIES_DIR)/aml_recovery/logo.img && 
+    fs/recovery_aml/$(IMGPACK) -r $(RES_PACK) $(BINARIES_DIR)/aml_recovery/logo.img && 
 
-ADDITIONAL_FILES += " logo.img"
+else
+
+ifneq ($(strip $(BR2_TARGET_ROOTFS_RECOVERY_AML_LOGO)),"")
+AML_LOGO = $(BR2_TARGET_ROOTFS_RECOVERY_AML_LOGO)
+else
+AML_LOGO = fs/recovery_aml/logos/mx_logo.img
 endif
+
+# Check if AML_LOGO exists
+$(if $(wildcard $(AML_LOGO)),,$(fatal AML_LOGO=$(AML_LOGO), file does not exist.))
+
+endif
+
+# Aditional files to be included in package, by default only logo.img
+ADDITIONAL_FILES = logo.img
 
 ###### Advanced options ######
 
 # Memory type
 ifeq ($(BR2_TARGET_ROOTFS_RECOVERY_AML_ADV_EMMC),y)
 RECOVERY_AML_ARGS += -m EMMC
-else
+else ifeq ($(BR2_TARGET_ROOTFS_RECOVERY_AML_ADV_MTD),y)
 RECOVERY_AML_ARGS += -m MTD
+else
+RECOVERY_AML_ARGS += -m UBI
 endif
 
 # File system for system and data partitions
 ifeq ($(BR2_TARGET_ROOTFS_RECOVERY_AML_ADV_FS_EXT4),y)
 RECOVERY_AML_ARGS += -f ext4
-else
+PARTITION_TYPE = ext4
+else ifeq ($(BR2_TARGET_ROOTFS_RECOVERY_AML_ADV_FS_YAFFS2),y)
 RECOVERY_AML_ARGS += -f yaffs2
+ifneq ($(BR2_BOARD_TYPE_AMLOGIC_M6),y)
+PARTITION_TYPE = legacy_yaffs2
+else
+PARTITION_TYPE = yaffs2
 endif
+else
+RECOVERY_AML_ARGS += -f ubifs
+PARTITION_TYPE = ubifs
+endif
+
+# Path to logo partition in recovery
+RECOVERY_AML_ARGS += -l $(BR2_TARGET_ROOTFS_RECOVERY_AML_ADV_PATH_LOGO)
 
 # Path to system partition in recovery
 RECOVERY_AML_ARGS += -s $(BR2_TARGET_ROOTFS_RECOVERY_AML_ADV_PATH_SYSTEM)
@@ -129,6 +154,16 @@ endif
 
 ###### Advanced options ######
 
+# If we have provided bootloader.img, make sure it's included in update.zip
+ifneq ($(strip $(BR2_TARGET_ROOTFS_RECOVERY_BOOTLOADER_IMG)),"")
+
+ROOTFS_RECOVERY_AML_CMD += \
+    echo "Copy bootloader.img..." && \
+    cp -f $(BR2_TARGET_ROOTFS_RECOVERY_BOOTLOADER_IMG) $(BINARIES_DIR)/aml_recovery/bootloader.img && 
+
+ADDITIONAL_FILES += " bootloader.img"
+endif
+
 # If we have provided recovery.img, make sure it's included in update.zip
 ifneq ($(strip $(BR2_TARGET_ROOTFS_RECOVERY_RECOVERY_IMG)),"")
 
@@ -139,14 +174,17 @@ ROOTFS_RECOVERY_AML_CMD += \
 ADDITIONAL_FILES += " recovery.img"
 endif
 
-# If we have provided bootloader.img, make sure it's included in update.zip
-ifneq ($(strip $(BR2_TARGET_ROOTFS_RECOVERY_BOOTLOADER_IMG)),"")
-
 ROOTFS_RECOVERY_AML_CMD += \
-    echo "Copy bootloader.img..." && \
-    cp -f $(BR2_TARGET_ROOTFS_RECOVERY_BOOTLOADER_IMG) $(BINARIES_DIR)/aml_recovery/bootloader.img && 
+    tar -C $(BINARIES_DIR)/aml_recovery/system -xf $(BINARIES_DIR)/rootfs.tar && \
+    sed -i -f fs/recovery_aml/$(PARTITION_TYPE).sed $(BINARIES_DIR)/aml_recovery/system/etc/init.d/S10setup && \
+    mkdir -p $(BINARIES_DIR)/aml_recovery/META-INF/com/google/android/ && \
+    PYTHONDONTWRITEBYTECODE=1 $(HOST_DIR)/usr/bin/python fs/recovery_aml/android_scriptgen $(RECOVERY_AML_ARGS) -i -p $(BINARIES_DIR)/aml_recovery/system -o \
+     $(BINARIES_DIR)/aml_recovery/META-INF/com/google/android/updater-script && \
+    cp -f fs/recovery_aml/update-binary $(BINARIES_DIR)/aml_recovery/META-INF/com/google/android/ &&
 
-ADDITIONAL_FILES += " bootloader.img"
+ifneq ($(BR2_TARGET_ROOTFS_RECOVERY_AML_IMGPACK),y)
+ROOTFS_RECOVERY_AML_CMD += \
+    cp -f $(AML_LOGO) $(BINARIES_DIR)/aml_recovery/logo.img &&
 endif
 
 ifneq ($(strip $(BR2_TARGET_ROOTFS_RECOVERY_AML_APPEND_INITRD)),"")
@@ -166,12 +204,6 @@ ROOTFS_RECOVERY_AML_CMD += \
 endif
 
 ROOTFS_RECOVERY_AML_CMD += \
-    tar -C $(BINARIES_DIR)/aml_recovery/system -xf $(BINARIES_DIR)/rootfs.tar && \
-    mkdir -p $(BINARIES_DIR)/aml_recovery/META-INF/com/google/android/ && \
-    PYTHONDONTWRITEBYTECODE=1 $(HOST_DIR)/usr/bin/python fs/recovery_aml/android_scriptgen $(RECOVERY_AML_ARGS) -i -p $(BINARIES_DIR)/aml_recovery/system -o \
-     $(BINARIES_DIR)/aml_recovery/META-INF/com/google/android/updater-script && \
-    cp -f fs/recovery_aml/update-binary $(BINARIES_DIR)/aml_recovery/META-INF/com/google/android/ && \
-    cp -f $(AML_LOGO) $(BINARIES_DIR)/aml_recovery/aml_logo.img && \
     find $(BINARIES_DIR)/aml_recovery/system/ -type l -delete && \
     find $(BINARIES_DIR)/aml_recovery/system/ -type d -empty -exec sh -c 'echo "dummy" > "{}"/.empty' \; && \
     pushd $(BINARIES_DIR)/aml_recovery/ >/dev/null && \
