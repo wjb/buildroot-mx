897fc165357f7ae00a3c1bba22b344bd1558270a
wjb <jaybob413@gmail.com>
2014-05-03 09:15:41 -0400

    [fs/cpio] add targets/processing for ...INITRAMFS_LIST / ...INITRAMFS_INIT

--- fs/cpio/cpio.mk
+++ fs/cpio/cpio.mk
@@ -15,9 +15,16 @@ endef
 else
 # devtmpfs does not get automounted when initramfs is used.
 # Add a pre-init script to mount it before running init
+
+ifneq ($(BR2_TARGET_ROOTFS_INITRAMFS_INIT),"")
+INITRAMFS_INIT = $(BR2_TARGET_ROOTFS_INITRAMFS_INIT)
+else
+INITRAMFS_INIT = fs/cpio/init
+endif
+
 define ROOTFS_CPIO_ADD_INIT
-        if [ ! -e $(TARGET_DIR)/init ]; then \
-                $(INSTALL) -m 0755 fs/cpio/init $(TARGET_DIR)/init; \
+        if [ ! -e $(INITRAMFS_INIT) ]; then \
+                $(INSTALL) -m 0755 $(INITRAMFS_INIT) $(TARGET_DIR)/init; \
         fi
 endef
 
@@ -27,9 +34,16 @@ endif # BR2_ROOTFS_DEVICE_CREATION_STATIC
 
 ROOTFS_CPIO_PRE_GEN_HOOKS += ROOTFS_CPIO_ADD_INIT
 
+
+ifneq ($(BR2_TARGET_ROOTFS_INITRAMFS_LIST),"")
+define ROOTFS_CPIO_CMD
+	cd $(TARGET_DIR) && cat $(CONFIG_DIR)/$(BR2_TARGET_ROOTFS_INITRAMFS_LIST) | cpio --quiet -o -H newc > $@
+endef
+else
 define ROOTFS_CPIO_CMD
 	cd $(TARGET_DIR) && find . | cpio --quiet -o -H newc > $@
 endef
+endif # BR2_TARGET_ROOTFS_INITRAMFS_LIST
 
 $(BINARIES_DIR)/rootfs.cpio.uboot: $(BINARIES_DIR)/rootfs.cpio host-uboot-tools
 	$(MKIMAGE) -A $(MKIMAGE_ARCH) -T ramdisk \
