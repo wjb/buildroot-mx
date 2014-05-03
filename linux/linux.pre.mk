1f353d6756b5aea84aee4cda5ff3b0c23aaa2f40
wjb <jaybob413@gmail.com>
2014-05-03 09:19:21 -0400

    [linux] add processing for prebuilt device tree blob

--- linux/linux.pre.mk
+++ linux/linux.pre.mk
@@ -12,6 +12,8 @@ MOD_IN = $(@D)/$(call qstrip,$(BR2_PREBUILT_LINUX_MODS))
 MOD_OUT = $(TARGET_DIR)/lib/modules
 IMAGE_IN = $(@D)/$(call qstrip,$(BR2_PREBUILT_LINUX_IMAGE))
 IMAGE_OUT = $(TOPDIR)/output/images
+DTB_IN = $(@D)/$(call qstrip,$(BR2_PREBUILT_LINUX_DTB))
+DTB_OUT = $(TOPDIR)/output/images
 
 define MODULE_CMDS
 if [ -e $(MOD_OUT) ]; then rm -rf $(MOD_OUT); fi;
@@ -24,9 +26,17 @@ if [ -e $(IMAGE_OUT)/$(call qstrip,$(BR2_PREBUILT_LINUX_IMAGE)) ]; then rm $(IMA
  cp $(IMAGE_IN) $(IMAGE_OUT)
 endef
 
+ifneq ($(strip $(BR2_PREBUILT_LINUX_DTB)),"")
+define DTB_CMDS
+if [ -e $(DTB_OUT)/$(call qstrip,$(BR2_PREBUILT_LINUX_DTB)) ]; then rm $(DTB_OUT)/$(call qstrip,$(BR2_PREBUILT_LINUX_DTB)); fi;
+ cp $(DTB_IN) $(DTB_OUT)
+endef
+endif
+
 define LINUX_INSTALL_TARGET_CMDS
 $(call MODULE_CMDS)
 $(call IMAGE_CMDS)
+$(call DTB_CMDS)
 endef
 
 $(eval $(generic-package))
